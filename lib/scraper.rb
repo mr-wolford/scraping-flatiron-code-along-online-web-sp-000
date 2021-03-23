29d8777b49095c0974c9d8ea04ebae112b13c1b4
Matt Wolford <matthew.r.wolford@gmail.com>
2021-03-23 01:11:05 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,16 +12,6 @@ class Scraper
     binding.pry
   end
 
-
-  def print_courses
-    self.make_courses
-    Course.all.each do |course|
-      if course.title && course.title != ""
-        puts "Title: #{course.title}"
-        puts "  Schedule: #{course.schedule}"
-        puts "  Description: #{course.description}"
-      end
-    end
-  end
-
 end
+
+Scraper.new.get_page
